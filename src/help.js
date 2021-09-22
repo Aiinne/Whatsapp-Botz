b686d33adc8ae1d8332d0b76a5fe2f783add5549
MITSUHA OFC <77813696+MITSUHA-OFC@users.noreply.github.com>
2021-09-22 18:57:14 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,234 @@
+const help = (ucapanWaktu, jam, wit, wita, pushname, tchat, role, bulan, prefix, owner) => {
+	return ` *${ucapanWaktu} Lord*
+	
+╭❖ ❰ *MITSUHA BOTz* ❱ ❖──────╮  
+┠❍ *Nama : ${pushname}*
+┠❍ *Pangkat : ${role}*
+┠❍ *Bulan : ${bulan}*
+┠❍ *Owner : ${owner}*
+┠❍ *Chat : ${tchat} pesan*
+┠❍ *Wib : Jam ${jam}*
+┠❍ *Wit : Jam ${wit}*
+┠❍ *Wita : Jam ${wita}*
+╰─────────────────────╯
+
+╭─❖ ❰ *INFO DEVELOPER* ❱ ❖───╮  
+┠❃ *Script Ori : Mhankbarbar*
+┠❃ *Developer : Chaca*
+┠❃ *Status : Online*
+┠❃ *Device : Redmi 6A*
+┠❃ *No : +62 857-3126-1728* 
+╰─────────────────────╯
+
+╭─❖ ❰ *INFORMASI BOT* ❱ ❖────╮  
+┠❃ *${prefix}owner*
+┠❃ *${prefix}bug*
+╰─────────────────────╯
+
+╭─❖ ❰ *ANIME MENU* ❱ ❖──────╮  
+┠❃ *${prefix}loli*
+┠❃ *${prefix}nekonime*
+┠❃ *${prefix}waifu
+┠❃ *${prefix}waifu2
+╰─────────────────────╯
+
+╭─❖ ❰ *MAKER MENU* ❱ ❖──────╮  
+┠❃ *${prefix}sticker*
+┠❃ *${prefix}stickergif*
+┠❃ *${prefix}ubahstk*
+┠❃ *${prefix}toimg*
+┠❃ *${prefix}tahta*
+┠❃ *${prefix}pornlogo*
+┠❃ *${prefix}epep*
+┠❃ *${prefix}text3d*
+╰─────────────────────╯
+
+╭─❖ ❰ *SOUND MENU* ❱ ❖──────╮  
+┠❃ *${prefix}sound1*
+┠❃ *${prefix}sound2*
+┠❃ *${prefix}sound3*
+┠❃ *${prefix}sound4*
+┠❃ *${prefix}sound5*
+┠❃ *${prefix}sound6*
+┠❃ *${prefix}sound7*
+┠❃ *${prefix}sound8*
+┠❃ *${prefix}sound9*
+┠❃ *${prefix}sound10*
+┠❃ *${prefix}sound11*
+┠❃ *${prefix}sound12*
+┠❃ *${prefix}sound13*
+┠❃ *${prefix}sound14*
+┠❃ *${prefix}sound15*
+┠❃ *${prefix}sound16*
+┠❃ *${prefix}sound17*
+┠❃ *${prefix}sound18*
+┠❃ *${prefix}sound19*
+┠❃ *${prefix}sound20*
+┠❃ *${prefix}sound21*
+┠❃ *${prefix}sound22*
+┠❃ *${prefix}sound23*
+┠❃ *${prefix}sound24*
+┠❃ *${prefix}sound25*
+┠❃ *${prefix}sound26*
+┠❃ *${prefix}sound27*
+┠❃ *${prefix}sound28*
+┠❃ *${prefix}sound29*
+┠❃ *${prefix}sound30*
+┠❃ *${prefix}sound31*
+┠❃ *${prefix}sound32*
+┠❃ *${prefix}sound33*
+┠❃ *${prefix}sound34*
+┠❃ *${prefix}sound35*
+┠❃ *${prefix}sound36*
+┠❃ *${prefix}sound37*
+┠❃ *${prefix}sound38*
+┠❃ *${prefix}sound39*
+┠❃ *${prefix}sound40*
+┠❃ *${prefix}sound41*
+┠❃ *${prefix}sound42*
+┠❃ *${prefix}sound43*
+┠❃ *${prefix}sound44*
+┠❃ *${prefix}sound45*
+┠❃ *${prefix}sound46*
+┠❃ *${prefix}sound47*
+┠❃ *${prefix}sound48*
+┠❃ *${prefix}sound49*
+┠❃ *${prefix}sound50*
+┠❃ *${prefix}sound51*
+┠❃ *${prefix}sound52*
+┠❃ *${prefix}sound53*
+┠❃ *${prefix}sound54*
+┠❃ *${prefix}sound55*
+┠❃ *${prefix}sound56*
+┠❃ *${prefix}sound57*
+┠❃ *${prefix}sound58*
+┠❃ *${prefix}sound59*
+┠❃ *${prefix}sound60*
+┠❃ *${prefix}sound61*
+┠❃ *${prefix}sound62*
+┠❃ *${prefix}sound63*
+┠❃ *${prefix}sound64*
+┠❃ *${prefix}sound65*
+┠❃ *${prefix}sound66*
+┠❃ *${prefix}sound67*
+┠❃ *${prefix}sound68*
+┠❃ *${prefix}sound69*
+┠❃ *${prefix}sound70*
+╰─────────────────────╯
+
+╭─❖ ❰ *STORAGE MENU* ❱ ❖────╮  
+┠❃ *${prefix}addsticker*
+┠❃ *${prefix}addimage*
+┠❃ *${prefix}addvn*
+┠❃ *${prefix}addvideo*
+┠❃ *${prefix}liststicker*
+┠❃ *${prefix}listimage*
+┠❃ *${prefix}listvn*
+┠❃ *${prefix}listvideo*
+┠❃ *${prefix}getsticker*
+┠❃ *${prefix}getimage*
+┠❃ *${prefix}getvn*
+┠❃ *${prefix}getvideo*
+╰─────────────────────╯
+
+╭─❖ ❰ *GRUP MENU* ❱ ❖───────╮  
+┠❃ *${prefix}linkgc*
+┠❃ *${prefix}add*
+┠❃ *${prefix}kick*
+┠❃ *${prefix}promote*
+┠❃ *${prefix}demote*
+┠❃ *${prefix}linkgroup*
+┠❃ *${prefix}tagall*
+┠❃ *${prefix}simih*
+┠❃ *${prefix}antilink*
+┠❃ *${prefix}antixnxx*
+┠❃ *${prefix}antiwibu*
+┠❃ *${prefix}antibocil*
+┠❃ *${prefix}antiyt*
+┠❃ *${prefix}antiig*
+╰─────────────────────╯
+
+╭─❖ ❰ *IMAGE MENU* ❱ ❖──────╮  
+┠❃ *${prefix}ocr*
+┠❃ *${prefix}wait*
+┠❃ *${prefix}url2img*
+╰─────────────────────╯
+
+╭─❖ ❰ *INDOHOT* ❱ ❖────────╮  
+┠❃ *${prefix}indo1*
+┠❃ *${prefix}indo2*
+┠❃ *${prefix}indo3*
+┠❃ *${prefix}indo4*
+┠❃ *${prefix}indo5*
+┠❃ *${prefix}indo6*
+┠❃ *${prefix}indo7*
+┠❃ *${prefix}indo8*
+┠❃ *${prefix}indo9*
+┠❃ *${prefix}indo10*
+┠❃ *${prefix}indo11*
+┠❃ *${prefix}indo12*
+┠❃ *${prefix}indo13*
+┠❃ *${prefix}indo14*
+┠❃ *${prefix}indo15*
+┠❃ *${prefix}indo16*
+┠❃ *${prefix}indo17*
+┠❃ *${prefix}indo18*
+┠❃ *${prefix}indo19*
+┠❃ *${prefix}indo20*
+┠❃ *${prefix}indo21*
+┠❃ *${prefix}indo22*
+┠❃ *${prefix}indo23*
+┠❃ *${prefix}indo24*
+┠❃ *${prefix}indo25*
+╰─────────────────────╯
+
+╭─❖ ❰ *WALLPAPER* ❱ ❖───────╮  
+┠❃ *${prefix}wallpaper1*
+┠❃ *${prefix}wallpaper2*
+┠❃ *${prefix}wallpaper3*
+┠❃ *${prefix}wallpaper4*
+┠❃ *${prefix}wallpaper5*
+┠❃ *${prefix}wallpaper6*
+┠❃ *${prefix}wallpaper7*
+┠❃ *${prefix}wallpaper8*
+┠❃ *${prefix}wallpaper9*
+┠❃ *${prefix}wallpaper10*
+╰─────────────────────╯
+
+╭─❖ ❰ *MEDIA MENU* ❱ ❖──────╮  
+┠❃ *${prefix}meme*
+┠❃ *${prefix}memeindo*
+┠❃ *${prefix}memeindo2*
+┠❃ *${prefix}darkjokes*
+┠❃ *${prefix}gtts*
+┠❃ *${prefix}pinterest*
+┠❃ *${prefix}pinterest2*
+╰─────────────────────╯
+
+╭─❖ ❰ *OTHER MENU* ❱ ❖──────╮  
+┠❃ *${prefix}delete*
+┠❃ *${prefix}cekchat*
+┠❃ *${prefix}role*
+╰─────────────────────╯
+
+╭─❖ ❰ *OWNER MENU* ❱ ❖──────╮
+  ┠❃ *${prefix}setwelcome
+┠❃ *${prefix}setleave
+┠❃ *${prefix}setprefix*
+┠❃ *${prefix}setcr*
+┠❃ *${prefix}setfake1*
+┠❃ *${prefix}setfake2*
+┠❃ *${prefix}setmenu*
+┠❃ *${prefix}leave*
+┠❃ *${prefix}clone*
+┠❃ *${prefix}bc*
+╰─────────────────────╯
+
+╭─❖ ❰ *THANKS TO* ❱ ❖───────╮  
+┠❍ *MhankBarBar*
+┠❍ *Oreo*
+┠❍ *Nayla*
+┠❍ *All Kreator Bot*
+╰─────────────────────╯`
+}
+exports.help = help
